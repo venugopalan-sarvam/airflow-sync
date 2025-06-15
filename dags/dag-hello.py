7205663fa1d843db564a6e884ac01849593f2164
venugopalan-sarvam <venugopalan@sarvam.ai>
2025-06-15 20:24:12 +0530

    checking sys path airflow is seeing

--- dags/dag-hello.py
+++ dags/dag-hello.py
@@ -1,33 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 import sys
-sys.path.append('/opt/airflow/git/airflow-sync.git/dags')
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from hellocall.hello import hello
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 5, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1
-}
+def print_sys_path():
+    print("PYTHONPATH:", sys.path)
 
-# Instantiate the DAG object
 with DAG(
-    'dags-greeting.py',
-    default_args=default_args,
-    description='A simple greeting DAG',
+    'print_python_path',
     schedule_interval=None,
+    start_date=datetime(2023, 1, 1),
     catchup=False
 ) as dag:
-    
-    # Define the tasks
-    hello_task = BashOperator(
-        task_id='hello_world_task',
-        bash_command=f'echo {hello("Venu")}'
+    t1 = PythonOperator(
+        task_id='print_path',
+        python_callable=print_sys_path
     )
-
-    # Set the task dependencies
-    hello_task
