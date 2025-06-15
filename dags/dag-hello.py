070926c464b050fee8ddfa502f2dc1c4f587d19f
venugopalan-sarvam <venugopalan@sarvam.ai>
2025-06-15 20:28:49 +0530

    listing all directories now

--- dags/dag-hello.py
+++ dags/dag-hello.py
@@ -1,18 +1,26 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-import sys
+import os
 
-def print_sys_path():
-    print("PYTHONPATH:", sys.path)
+def list_files_and_folders():
+    root_dir = '/opt/airflow/git/airflow-sync.git/dags'
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        print(f"Directory: {dirpath}")
+        if dirnames:
+            print(f"  Subdirectories: {dirnames}")
+        if filenames:
+            print(f"  Files: {filenames}")
+        print("----")
 
 with DAG(
-    'print_python_path',
+    'list_airflow_dags_folder',
     schedule_interval=None,
     start_date=datetime(2023, 1, 1),
-    catchup=False
+    catchup=False,
+    tags=['debug']
 ) as dag:
-    t1 = PythonOperator(
-        task_id='print_path',
-        python_callable=print_sys_path
+    list_files_task = PythonOperator(
+        task_id='list_files_and_folders_task',
+        python_callable=list_files_and_folders
     )
