7a26862e7666a35752f92bec149985c3a9869326
Venugopalan Iyengar <venugopalan@sarvam.ai>
2025-06-15 21:32:09 +0530

    feat: Deploy Airflow DAG 'testpandas_yes_1.py' and script 'yes_1.py' via Streamlit

---
+++ dags/hellocall/yes_1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+def hello(name: str) -> str:
+    df = pd.DataFrame()
+    greeting = f"Hello {name}! Good day to you!"
+    print(greeting)
+    return greeting
+
+def run_script_main():
+    hello("Venugopalan")
+
+run_script_main()
