92c24645d9d334b4a6e03d997f323eca45f9740b
Venugopalan Iyengar <venugopalan@sarvam.ai>
2025-06-15 21:25:50 +0530

    feat: Deploy Airflow DAG 'testhellodag_yes.py' and script 'yes.py' via Streamlit

---
+++ dags/testhellodag_yes.py
@@ -0,0 +1,29 @@
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+import sys
+import os
+
+with DAG(
+    # Ensure this DAG ID is unique within your Airflow environment
+    dag_id='testhellodag_yes',
+    start_date=datetime(2023, 1, 1), # Consider setting this to a date in the past
+    schedule_interval='0 1 * * *',
+    catchup=False,
+    tags=['user_upload', 'dynamic'],
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    }
+) as dag:
+    hello_task = BashOperator(
+        task_id='run_uploaded_script_yes',
+        bash_command='python /opt/airflow/git/airflow-sync.git/dags/hellocall/yes.py'
+    )
+
+    hello_task
