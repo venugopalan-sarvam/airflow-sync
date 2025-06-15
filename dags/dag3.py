90c78e385e58d701a4e7a831b7308ff42c6b2d21
venugopalan-sarvam <venugopalan@sarvam.ai>
2025-06-15 19:59:22 +0530

    greeting from another file

---
+++ dags/dag3.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from hellocall import hello
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 5, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1
+}
+
+# Instantiate the DAG object
+with DAG(
+    'dags-greeting.py',
+    default_args=default_args,
+    description='A simple greeting DAG',
+    schedule_interval=None,
+    catchup=False
+) as dag:
+    
+    # Define the tasks
+    hello_task = BashOperator(
+        task_id='hello_world_task',
+        bash_command=f'echo {hello("Venu")}'
+    )
+
+    # Set the task dependencies
+    hello_task
---
+++ dags/hellocall.py
@@ -0,0 +1,4 @@
+def hello(name: str) -> str:
+    greeting = f"Hello {name}! Good day to you!"
+    print(greeting)
+    return greeting
